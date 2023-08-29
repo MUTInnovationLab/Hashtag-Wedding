49757bdccdb4703617f7dac57495a3beb9f20d52
vusumuzi <vgwala149@gmail.com>
2023-08-29 15:18:27 +0200

    authicaticate from page to page

--- src/app/edit/edit.page.ts
+++ src/app/edit/edit.page.ts
@@ -1,7 +1,14 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IonButton } from '@ionic/angular';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 import { DataService } from '../services/data.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-edit',
@@ -11,12 +18,11 @@ import { DataService } from '../services/data.service';
     trigger('fadeInOut', [
       state('visible', style({ opacity: 1 })),
       state('hidden', style({ opacity: 0 })),
-      transition('visible <=> hidden', animate('1s ease-in-out'))
-    ])
-  ]
+      transition('visible <=> hidden', animate('1s ease-in-out')),
+    ]),
+  ],
 })
 export class EditPage implements OnInit {
-
   showProfile = true;
   showWeddingInfo = false;
 
@@ -32,17 +38,16 @@ export class EditPage implements OnInit {
       if (!this.showProfile && this.saveButton) {
         this.saveButton.nativeElement.focus();
       }
-    },1000);
+    }, 1000);
   }
-
 
   user = {
     name: '',
     surname: '',
     country: '',
-    email:'',
-    phone:'',
-    gender:''
+    email: '',
+    phone: '',
+    gender: '',
 
     // Other profile fields here
   };
@@ -56,67 +61,100 @@ export class EditPage implements OnInit {
     days_Till_wed: 0,
     location: '',
     minBudget: '',
-    maxBudget: ''
+    maxBudget: '',
   };
 
-
-  constructor(private dataService:DataService) { }
+  constructor(private dataService: DataService,private auth:AngularFireAuth) {}
 
   ngOnInit() {
+    this.getUserDoc();
   }
 
   saveWeddingChanges() {
-    this.wedding.couple=this.wedding.groom + " & " + this.wedding.bride;
+    this.wedding.couple = this.wedding.groom + ' & ' + this.wedding.bride;
     const combinedData = {
       user: this.user,
-      wedding: this.wedding
+      wedding: this.wedding,
     };
-    this.dataService.saveDetails(combinedData).then(response => {
-      alert("data saved");
-      this.user = {
-        name: '',
-        surname: '',
-        country: '',
-        email:'',
-        phone:'',
-        gender:''
-    
-       // Other profile fields here
-      };
-      this.wedding = {
-        wed_type: '',
-        c_gender: '',
-        groom: '',
-        bride: '',
-        couple: '',
-        wed_date: '',
-        days_Till_wed: 0,
-        location: '',
-        minBudget: '',
-        maxBudget: ''
-      };
+    this.dataService
+      .saveDetails(combinedData)
+      .then((response) => {
+        alert('data saved');
+        this.user = {
+          name: '',
+          surname: '',
+          country: '',
+          email: '',
+          phone: '',
+          gender: '',
+
+          // Other profile fields here
+        };
+        this.wedding = {
+          wed_type: '',
+          c_gender: '',
+          groom: '',
+          bride: '',
+          couple: '',
+          wed_date: '',
+          days_Till_wed: 0,
+          location: '',
+          minBudget: '',
+          maxBudget: '',
+        };
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }
+
+
+  async getUserDoc() {
+    try {
+      const user = await this.auth.currentUser;
+  
+      if (!user) {
+        console.log("User is not authenticated.");
+        return;
+      }
+  
+      if (!user.email) {
+        console.log("User's email is not available.");
+        return;
+      }
+  
+      const docSnapshot = await this.dataService.getUserDoc(user.email);
+  
+      if (docSnapshot?.exists) {
+        console.log("Document data:", docSnapshot.data());
+      } else {
+        console.log("Document does not exist.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  }
   
 
 
-    }).catch(error=>{
-      console.error(error.message);
-    })
-  }
 
-  setDate(e:any){
 
-      this.wedding.wed_date=e.detail.value.split('T')[0];
-      this.calculateDaysLeft();
 
+ 
+  
+  
+  
+  setDate(e: any) {
+    this.wedding.wed_date = e.detail.value.split('T')[0];
+    this.calculateDaysLeft();
   }
   calculateDaysLeft() {
     const weddingDate = new Date(this.wedding.wed_date);
     const today = new Date();
-    
+
     const timeDifference = weddingDate.getTime() - today.getTime();
     const daysLeft = Math.ceil(timeDifference / (1000 * 3600 * 24));
-    
+
     this.wedding.days_Till_wed = daysLeft;
   }
-
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,21 +2,30 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private firestore:AngularFirestore,private auth:AngularFireAuth) { }
+  constructor(private firestore:AngularFirestore,private auth:AngularFireAuth) {}
 
   saveDetails(data:any ){
     return this.firestore.collection("userDetails").doc(data.user.email).update(data);
   }
-  getUserDoc(data:any ){
-    return this.firestore.collection("userDetails").doc(data.user.email).update(data);
+  getUserDoc(email: string) {
+    return this.firestore.collection("userDetails").doc(email).get().toPromise();
   }
-
- 
+  
+  async getCurrentUserEmail() {
+    const user = await this.auth.currentUser;
+    if (user) {
+      return user.email;
+    } else {
+      return null; // Or any appropriate value indicating no user
+    }
+  }
+  
 
 
 }
