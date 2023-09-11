2852cd8e83ff0b809dcb61bdc5a5b27cf41922fa
unknown <nduduzondlovu635@gmail.com>
2023-09-11 15:56:32 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,46 +1,9 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-
-// const routes: Routes = [
-//   {
-//     path: 'home',
-//     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-//   },
-//   {
-//     path: '',
-//     redirectTo: 'home',
-//     pathMatch: 'full'
-//   },
-//   {
-//     path: 'register',
-//     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
-//   },
-//   {
-//     path: 'login',
-//     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-//   },
-//   {
-//     path: 'profile',
-//     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
-//   },
-//   {
-//     path: 'edit',
-//     loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)
-//   },
-//   {
-//     path: 'view-profile',
-//     loadChildren: () => import('./view-profile/view-profile.module').then( m => m.ViewProfilePageModule)
-//   },
-//   {
-//     path: 'edit-profile',
-//     loadChildren: () => import('./edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
-//   },
-
-
 const routes: Routes = [
   {
-    path: '',
+    path: 'view-profile',
     loadChildren: () => import('./view-profile/view-profile.module').then( m => m.ViewProfilePageModule)
   },
   {
@@ -49,7 +12,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'budget',
     pathMatch: 'full'
   },
   {
@@ -82,7 +45,7 @@ const routes: Routes = [
     loadChildren: () => import('./checklist/checklist.module').then( m => m.ChecklistPageModule)
   },
   {
-    path: 'budget',
+    path: '',
     loadChildren: () => import('./budget/budget.module').then( m => m.BudgetPageModule)
   },
 
--- src/app/budget/budget.page.ts
+++ src/app/budget/budget.page.ts
@@ -5,11 +5,45 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './budget.page.html',
   styleUrls: ['./budget.page.scss'],
 })
-export class BudgetPage implements OnInit {
+export class BudgetPage {
+  budgetItems = [
+    { name: 'Venue Rental Fee', budgetAmount: 2000.00, actualAmount: 0.00 },
+    { name: "Officiant's Fee", budgetAmount: 500.00, actualAmount: 0.00 },
+    { name: 'Marriage License', budgetAmount: 100.00, actualAmount: 0.00 },
+    // Add more budget items with default values as needed
 
-  constructor() { }
+    { name: 'Reception - Venue', budgetAmount: 3000.00, actualAmount: 0.00 },
+    { name: 'Reception - Food', budgetAmount: 4000.00, actualAmount: 0.00 },
+    { name: 'Reception - Cake', budgetAmount: 500.00, actualAmount: 0.00 },
+    { name: 'Reception - Bar Service', budgetAmount: 800.00, actualAmount: 0.00 }
+  ];
 
-  ngOnInit() {
+  receptionBudgetItems = [
+    { name: 'Reception - Venue', budgetAmount: 3000.00, actualAmount: 0.00 },
+    { name: 'Reception - Food', budgetAmount: 4000.00, actualAmount: 0.00 },
+    { name: 'Reception - Cake', budgetAmount: 500.00, actualAmount: 0.00 },
+    { name: 'Reception - Bar Service', budgetAmount: 800.00, actualAmount: 0.00 },
+    // Add more reception budget items with default values as needed
+  ];
+
+  constructor() {}
+
+  // Calculate the total budget
+  totalBudget(): number {
+    return this.budgetItems.reduce((total, item) => total + item.budgetAmount, 0);
   }
 
+  // Calculate the total actual amount
+  totalActual(): number {
+    return this.budgetItems.reduce((total, item) => total + item.actualAmount, 0);
+  }
+  
+  totalReceptionBudget(): number {
+    return this.receptionBudgetItems.reduce((total, item) => total + item.budgetAmount, 0);
+  }
+
+  // Calculate the total actual amount for the reception
+  totalReceptionActual(): number {
+    return this.receptionBudgetItems.reduce((total, item) => total + item.actualAmount, 0);
+  }
 }
